bb073d257f6ac1d1f86087f2415e9173d894cac1
RennisOfficial <thedevilishduck@gmail.com>
2017-11-22 14:58:58 -0600

    added some melee weapons for real this time

--- weapon.py
+++ weapon.py
@@ -1,4 +1,4 @@
-from .items import Item
+from items import Item
 
 class Weapon(Item):
     """Base Class for weapons"""
@@ -14,4 +14,41 @@ class Weapon(Item):
 
     def getDescription(self):
         return self.description
+
+"""Melee"""
     
+class TurdSword(Weapon):
+    def __init__(self):
+        super().__init__(name="Turd Sword",
+                        description="A hand-crafted sword that deals very little damage. Kinda smells bad, too.",
+                        damage=1)
+                        
+class Stick(Weapon):
+    def __init__(self):
+        super().__init__(name="Stick",
+                        description="A flimsy stick that fell down from a tree one day and instills terror on anything smaller than an ant.",
+                        damage=2)
+
+class SeriousStick(Weapon):
+    def __init__(self):
+        super().__init__(name="Serious Stick",
+                        description="A solid stick that knows what it's doing and doesn't hold back when it comes to dealing some serious blows!",
+                        damage=5)
+
+class NerphSword(Weapon):
+    def __init__(self):
+        super().__init__(name="Nerph Sword",
+                        description="A foam sword on the outside. On the inside, there lies a brutal plastic stick that'll sting real bad.",
+                        damage=8)
+
+class SwordSword(Weapon):
+    def __init__(self):
+        super().__init__(name="Sword Sword",
+                        description="An actually metal sword that seemingly has had some okay blacksmithing put into it. It's about as sharp as it tastes.",
+                        damage=14)
+
+class AuthoritySword(Weapon):
+    def __init__(self):
+        super().__init__(name="Authority Sword",
+                        description="This sword is a sword that ensures its swordliness by behaving like a sword.",
+                        damage=18)
