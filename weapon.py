4fa96929a4e0a352d55ac66bcbf11d4ff5b5a7e4
RennisOfficial <thedevilishduck@gmail.com>
2017-11-22 13:32:44 -0600

    Created weapons class

---
+++ weapon.py
@@ -0,0 +1,16 @@
+class Weapon():
+
+    def __init__(name, damage, description):
+        self.name = name
+        self.damage = damage
+        self.description = description
+    
+    def getName():
+        return name
+
+    def getDamage():
+        return damage
+
+    def getDescription():
+        return description
+    
